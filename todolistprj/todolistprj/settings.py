bb6668752b601ab0f75741716aaca7d771a2bce7
loetitia <audiger.loetitia@gmail.com>
2022-11-12 15:37:00 +0100

    secure

--- todolistprj/todolistprj/settings.py
+++ todolistprj/todolistprj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1^d2h%w^6=%7fjm#5k^b_o^q)pj&!bflu#(c8i6hya9-!77_8f'
+SECRET_KEY = 'SK'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
